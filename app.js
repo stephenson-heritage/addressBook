a85ed693f3fec569d2c0696b5166a3312240391f
Stephenson Lucas <lstephenson@cegep-heritage.qc.ca>
2019-05-27 08:45:59 -0400

    with cors

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const cookieParser = require("cookie-parser");
 const debug = require("debug")("express:server");
 const bodyParser = require("body-parser");
 const app = express();
-const dbLayer = require("./config/db");
 const cors = require("cors");
 
+const dbLayer = require("./config/db");
 const addressBookRouter = require("./routes/api/addressBook");
 
 const port = 9000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
 app.options("*", cors());
+
 app.use(cookieParser());
 
 app.use("/inc", express.static(path.join(__dirname, "inc")));
