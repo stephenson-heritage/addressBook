dc1c54e624d79ee79f1936b511b3f4aed7240f00
Stephenson Lucas <lstephenson@cegep-heritage.qc.ca>
2019-05-28 11:59:04 -0400

    show address on map, add address

--- sampleclient/js/addressBook.js
+++ sampleclient/js/addressBook.js
@@ -1,21 +1,89 @@
+mapboxgl.accessToken =
+	"pk.eyJ1Ijoic3RlcGhlbnNvbi1oZXJpdGFnZSIsImEiOiJjanZ4ejlxazMwYWRlNDhrOHJxN2hlZGl5In0.GvwpDRkNHQKPfS8S2SA4Dg";
+
+const mapboxStyle =
+	"mapbox://styles/stephenson-heritage/cjw6lp2dl00yy1cs5gpqzbikd";
+
 const addressBook = {
-	list: "http://localhost:9000/api/addressbook/"
+	list: "http://localhost:9000/api/addressbook/",
+	add: "http://localhost:9000/api/addressbook/addAddress/"
 };
 
-const entryClick = el => {
-	console.log(el.dataset["lat"], el.dataset["lng"], el.id);
+const toggleAddDialog = show => {
+	document.getElementById("addEntry").style.display = show ? "block" : "none";
+};
+
+const initMap = container => {
+	const map = new mapboxgl.Map({
+		container: container,
+		style: mapboxStyle,
+		center: [-121.657481, 37.440222],
+		zoom: 16.5
+	});
+	return map;
+};
+
+const entryClick = (map, el) => {
+	//console.log(el.dataset["lat"], el.dataset["lng"], el.id);
+	if (el.dataset["lat"] !== undefined && el.dataset["lng"] !== undefined) {
+		map.easeTo({ center: [el.dataset["lng"], el.dataset["lat"]] });
+	}
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
+	// get container for addresses
 	const elPeople = document.getElementById("people");
-
+	// el to optimize rendering
 	const parent = document.createElement("span");
+
+	// a button on the entry form to save entry
+	const btnAddEntryEl = document.getElementById("btnEntryFormAdd");
+
+	let map = initMap("map");
+	let markers = [];
+
+	const mapEl = document.getElementById("map");
+
+	btnAddEntryEl.addEventListener("click", async () => {
+		let formEls = document.querySelectorAll(
+			"#entryFormForm input[type='text']"
+		);
+
+		let postData = {};
+		for (let i = 0; i < formEls.length; i++) {
+			if (formEls[i].value.trim() !== "") {
+				postData[formEls[i].name] = formEls[i].value;
+			}
+		}
+
+		const data = await fetch(addressBook.add, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(postData)
+		});
+		await data.json();
+
+		toggleAddDialog(false);
+	});
 
 	const data = await fetch(addressBook.list);
 	const dataJson = await data.json();
 
 	dataJson.forEach(element => {
-		console.log(element);
+		//console.log(element);
+
+		if (element.latlng) {
+			let tmpMarker = new mapboxgl.Marker()
+				.setLngLat([
+					element.latlng.coordinates[1],
+					element.latlng.coordinates[0]
+				])
+				.addTo(map);
+			markers.push(tmpMarker);
+		}
 		const tEl = document.createElement("div");
 		tEl.id = element.personId;
 		if (element.latlng !== null) {
@@ -50,7 +118,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 		tEl.appendChild(address);
 
 		tEl.addEventListener("click", () => {
-			entryClick(tEl);
+			entryClick(map, tEl);
 		});
 
 		parent.appendChild(tEl);
